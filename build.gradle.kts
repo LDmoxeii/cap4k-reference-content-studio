dc58112533ff711087f6d5b35fd5fd150de9d606
zhenyu.jiang <jiangzhenyu@keyfil.com>
2026-05-08 23:25:38 +0800

    build: fix shared plugin and adapter dependency management

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 plugins {
     base
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.spring) apply false
+    alias(libs.plugins.kotlin.jpa) apply false
+    alias(libs.plugins.spring.boot) apply false
+    alias(libs.plugins.spring.dependency.management) apply false
 }
 
 allprojects {
--- cap4k-reference-content-studio-adapter/build.gradle.kts
+++ cap4k-reference-content-studio-adapter/build.gradle.kts
@@ -2,6 +2,13 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.spring)
     alias(libs.plugins.kotlin.jpa)
+    alias(libs.plugins.spring.dependency.management)
+}
+
+dependencyManagement {
+    imports {
+        mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+    }
 }
 
 dependencies {
