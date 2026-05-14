4d7696fb008d160880c75f9fd3e7e37f3bcdeaf8
zhenyu.jiang <15976781430@163.com>
2026-05-14 22:38:48 +0800

    build: consume cap4k 0.6.0 from maven central

--- cap4k-reference-content-studio-application/build.gradle.kts
+++ cap4k-reference-content-studio-application/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 dependencies {
     implementation(platform("org.springframework.boot:spring-boot-dependencies:${libs.versions.spring.boot.get()}"))
     implementation(project(":cap4k-reference-content-studio-domain"))
-    implementation("com.only4:ddd-core:0.5.0-SNAPSHOT")
-    implementation("com.only4:ddd-domain-repo-jpa:0.5.0-SNAPSHOT")
+    implementation(libs.ddd.core)
+    implementation(libs.ddd.domain.repo.jpa)
     implementation("jakarta.validation:jakarta.validation-api:3.0.2")
     implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
     implementation(kotlin("reflect"))
