a4310972605eb19ced2ab15a6297814f65d6ad1f
zhenyu.jiang <15976781430@163.com>
2026-05-24 15:15:37 +0800

    chore: model strong ids in design contracts

--- cap4k-reference-content-studio-adapter/src/main/kotlin/com/only4/cap4k/reference/contentstudio/adapter/portal/api/payload/content/read/GetContentDetailPayload.kt
+++ cap4k-reference-content-studio-adapter/src/main/kotlin/com/only4/cap4k/reference/contentstudio/adapter/portal/api/payload/content/read/GetContentDetailPayload.kt
@@ -1,12 +1,10 @@
 
 package com.only4.cap4k.reference.contentstudio.adapter.portal.api.payload.content.read
 
+import com.only4.cap4k.reference.contentstudio.domain.aggregates.content.ContentId
+import com.only4.cap4k.reference.contentstudio.domain.shared.ids.ReviewerId
 import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
-import com.only4.cap4k.reference.contentstudio.domain.aggregates.content.ContentId
-import com.only4.cap4k.reference.contentstudio.domain.aggregates.media_processing_task.MediaProcessingTaskId
-import com.only4.cap4k.reference.contentstudio.domain.aggregates.paid_publication_task.PaidPublicationTaskId
-import com.only4.cap4k.reference.contentstudio.domain.shared.ids.ReviewerId
 
 object GetContentDetailPayload {
 
